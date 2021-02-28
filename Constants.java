fa0461832b2adc3ac6fde3270ca4ee08fad2a4bb
christina <MISHINA.7.5@yandex.ru>
2021-03-01 00:31:15 +0300

    Задача №7_ Циклы - исправлено

--- Constants.java
+++ Constants.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 public class Constants {
     public static void main(String[] args) {
-        int x = 3;
-        int y = 21;
-        int z = 13;
         Scanner in = new Scanner(System.in);
+        System.out.println("Введите константу X");
+        String conx = in.nextLine();
+        System.out.println("Введите константу Y");
+        String cony = in.nextLine();
+        System.out.println("Введите константу Z");
+        String conz = in.nextLine();
         System.out.println("Введите размер массива");
         int size = in.nextInt();
-        int[] nums = new int[size];
+        in.nextLine();
+        String[] nums = new String[size];
         for (int i = 0; i < size; i++) {
-            System.out.println("Введите число");
-            nums[i] = in.nextInt();
+            System.out.println("Введите значение");
+            nums[i] = in.nextLine();
         }
         for (int i = 0; i < size; i++) {
-            if (nums[i] == x ||  nums[i] == y ||  nums[i] == z ) {
+            if (nums[i].equals(conx) || nums[i].equals(cony) || nums[i].equals(conz) ) {
                 System.out.println("Данное значение имеется в константах");
                 return;
             }
